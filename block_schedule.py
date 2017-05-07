734396e136dd3f97bdbb60ef725ae1b8bf1acc12
Ilya Pashchenko <in4pashchenko@gmail.com>
2017-05-07 12:36:25 +0300

    AUC=0.7 only for RF

--- block_schedule.py
+++ block_schedule.py
@@ -6,14 +6,19 @@ from astropy import units as u
 from astropy.coordinates import SkyCoord
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, cross_val_predict
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.preprocessing import (PolynomialFeatures, StandardScaler,
+                                   LabelEncoder, OneHotEncoder)
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
-from sklearn.cross_validation import StratifiedKFold
+from sklearn.cross_validation import StratifiedKFold, cross_val_score
 from hyperopt import hp, fmin, tpe, STATUS_OK, Trials, space_eval
+from sklearn.feature_extraction import DictVectorizer
 
 
 
@@ -317,45 +322,136 @@ def add_distance_by_uttime(target_df, orbit_df):
         Updated examples data frame.
     """
     distances = list()
+    statuses = list()
+    row_before = None
     for index, row in target_df.iterrows():
         ut_dt = row['ut_dt']
         dist_km = orbit_df.ix[(orbit_df.ut_time-ut_dt).abs().argsort()[:1]].dist_km.values[0]
         distances.append(dist_km)
+        if row_before is not None:
+            ut_dt_before = row_before['ut_dt']
+            dist_km_before = orbit_df.ix[(orbit_df.ut_time-ut_dt_before).abs().argsort()[:1]].dist_km.values[0]
+        else:
+            dist_km_before = dist_km
+        if dist_km >= dist_km_before:
+            status = 'out'
+        else:
+            status = 'in'
+        row_before = row
+        statuses.append(status)
+
     target_df['dist_km'] = distances
+    target_df['status'] = statuses
 
     return target_df
 
 
+def plot_importance(clf, names):
+    """
+    Plot features importance of classifier.
+    
+    :param clf: 
+        Instance of classifier with ``feature_importances_`` attribute.
+    :param names: 
+        Iterable of features names.
+    :return: 
+        Figure with plot.
+    """
+    # sort importances
+    indices = np.argsort(clf.feature_importances_)
+    # plot as bar chart
+    fig, ax = plt.subplots()
+    ax.barh(np.arange(len(names)), clf.feature_importances_[indices])
+    ax.set_yticks(np.arange(len(names)) + 0.25)
+    ax.set_yticklabels(np.array(names)[indices])
+    ax.set_xlabel('Relative importance')
+    fig.show()
+    return fig
+
+
+def encode_onehot(df, cols):
+    """
+    One-hot encoding is applied to columns specified in a pandas DataFrame.
+    
+    Modified from: https://gist.github.com/kljensen/5452382
+    
+    Details:
+    
+    http://en.wikipedia.org/wiki/One-hot
+    http://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.OneHotEncoder.html
+    
+    @param df pandas DataFrame
+    @param cols a list of columns to encode
+    @return a DataFrame with one-hot encoding
+    """
+    vec = DictVectorizer()
+
+    vec_data = pd.DataFrame(vec.fit_transform(df[cols].to_dict(orient='records')).toarray())
+    vec_data.columns = vec.get_feature_names()
+    vec_data.index = df.index
+
+    df = df.drop(cols, axis=1)
+    df = df.join(vec_data)
+    return df
+
+
+def one_hot(df, cols):
+    """
+    The same as in encode_onehot by using ``Pandas`` machinery.
+    """
+    for each in cols:
+        dummies = pd.get_dummies(df[each], prefix=each, drop_first=False)
+        df = pd.concat([df, dummies], axis=1)
+    return df
+
+
 def objective(space):
     pprint.pprint(space)
-    clf = LogisticRegression(C=space['C'],
-                             class_weight={0: 1, 1: space['cw']},
-                             random_state=1, max_iter=300, n_jobs=1,
-                             tol=10.**(-5), penalty='l2')
+    # clf = LogisticRegression(C=space['C'],
+    #                          class_weight={0: 1, 1: space['cw']},
+    #                          random_state=1, max_iter=300, n_jobs=1,
+    #                          tol=10.**(-5), penalty='l2')
+    clf = RandomForestClassifier(n_estimators=space['n_estimators'],
+                                 max_depth=space['max_depth'],
+                                 max_features=space['max_features'],
+                                 min_samples_split=space['mss'],
+                                 min_samples_leaf=space['msl'],
+                                 class_weight={0: 1, 1: space['cw']},
+                                 verbose=1, random_state=1, n_jobs=4)
+    # clf = SVC(C=space['C'], class_weight={0: 1, 1: space['cw']},
+    #           probability=False, gamma=space['gamma'], random_state=1)
+    # clf = KNeighborsClassifier(n_neighbors=space['k'], weights='distance',
+    #                            n_jobs=2)
     estimators = list()
-    estimators.append(('poly', PolynomialFeatures()))
-    estimators.append(('scaler', StandardScaler()))
+    # estimators.append(('poly', PolynomialFeatures()))
+    # estimators.append(('scaler', StandardScaler()))
     estimators.append(('clf', clf))
     pipeline = Pipeline(estimators)
 
-    y_preds = cross_val_predict(pipeline, X, y, cv=kfold, n_jobs=4)
-    CMs = list()
-    for train_idx, test_idx in kfold:
-        CMs.append(confusion_matrix(y[test_idx], y_preds[test_idx]))
-    CM = np.sum(CMs, axis=0)
+    auc = np.mean(cross_val_score(pipeline, X, y, cv=kfold, scoring='roc_auc',
+                                  verbose=1, n_jobs=1))
+    # y_preds = cross_val_predict(pipeline, X, y, cv=kfold, n_jobs=1)
+    # CMs = list()
+    # for train_idx, test_idx in kfold:
+    #     CMs.append(confusion_matrix(y[test_idx], y_preds[test_idx]))
+    # CM = np.sum(CMs, axis=0)
 
-    FN = CM[1][0]
-    TP = CM[1][1]
-    FP = CM[0][1]
-    print("TP = {}".format(TP))
-    print("FP = {}".format(FP))
-    print("FN = {}".format(FN))
+    # FN = CM[1][0]
+    # TP = CM[1][1]
+    # FP = CM[0][1]
+    # print("TP = {}".format(TP))
+    # print("FP = {}".format(FP))
+    # print("FN = {}".format(FN))
 
-    f1 = 2. * TP / (2. * TP + FP + FN)
+    # f1 = 2. * TP / (2. * TP + FP + FN)
+    # beta = 0.25
+    # fbeta = (1+beta**2)*TP/((1+beta**2)*TP+FN*beta**2+FP)
 
-    print("F1: {}".format(f1))
+    # print("F1: {}".format(f1))
+    # print("F0.25: {}".format(fbeta))
+    print("AUC: {}".format(auc))
 
-    return{'loss': 1-f1, 'status': STATUS_OK}
+    return{'loss': 1-auc, 'status': STATUS_OK}
 
 
 if __name__ == '__main__':
@@ -371,6 +467,11 @@ if __name__ == '__main__':
                                            'RA_cat_v054.pkl')
     fr = add_distance_by_uttime(fr, orbit_df)
 
+    # One-hot encode preceding/receding SRT
+    fr = one_hot(fr, ['status'])
+    # Delete encoded column
+    del fr['status']
+
     # Move RA, DEC to rad
     fr['ra'] = fr['ra'].apply(lambda ra: ra.wrap_at(180 * u.deg).radian)
     fr['dec'] = fr['dec'].apply(lambda dec: dec.radian)
@@ -383,9 +484,10 @@ if __name__ == '__main__':
     fr['cosra'] = fr['ra'].apply(lambda ra: np.cos(ra))
 
     # Create arrays of features and responces
-    # features_names = ['frac_year', 'sindec', 'sinra', 'cosra']
-    # X = np.array(fr[list(features_names)].values, dtype=float)
-    # y = np.array(fr['rank'].values, dtype=int)
+    features_names = ['frac_year', 'sindec', 'sinra', 'cosra', 'dist_km',
+                      'status_in', 'status_out']
+    X = np.array(fr[list(features_names)].values, dtype=float)
+    y = np.array(fr['rank'].values, dtype=int)
 
     # Just naively try kNN
     # clf = KNeighborsClassifier(n_neighbors=15, weights='distance', n_jobs=2)
@@ -396,16 +498,35 @@ if __name__ == '__main__':
     # print(classification_report(y_test, y_pred))
 
     # Tuning HP of logistic regression with polynomial features
-    # kfold = StratifiedKFold(y, n_folds=4, shuffle=True, random_state=1)
     # space = {'C': hp.loguniform('C', -3.3, 6.3),
     #          'cw': hp.loguniform('cw', -0.7, 5)}
-    # trials = Trials()
-    # best = fmin(fn=objective,
-    #             space=space,
-    #             algo=tpe.suggest,
-    #             max_evals=500,
-    #             trials=trials)
-    # pprint.pprint(space_eval(space, best))
+
+    # Tuning HP of RF classifier
+    space = {'n_estimators': hp.choice('n_estimators', np.arange(600, 1500, 100,
+                                                                 dtype=int)),
+             'max_depth': hp.choice('max_depth', np.arange(8, 15, dtype=int)),
+             'max_features': hp.choice('max_features', np.arange(2, 5, dtype=int)),
+             'mss': hp.choice('mss', np.arange(15, 30, 2, dtype=int)),
+             'cw': hp.uniform('cw', 2, 8),
+             'msl': hp.choice('msl', np.arange(2, 10, dtype=int))}
+
+    # Tuning HP of RBF-SVM
+    # space = {'C': hp.loguniform('C', -3.0, 5.6),
+    #          'gamma': hp.loguniform('gamma', -6.2, 4.6),
+    #          'cw': hp.uniform('cw', 0.5, 10)}
+
+    # Tuning HP of kNN
+    # space = {'k': hp.choice('k', np.arange(1, 40, dtype=int))}
+
+    kfold = StratifiedKFold(y, n_folds=4, shuffle=True, random_state=1)
+
+    trials = Trials()
+    best = fmin(fn=objective,
+                space=space,
+                algo=tpe.suggest,
+                max_evals=200,
+                trials=trials)
+    pprint.pprint(space_eval(space, best))
 
 
     # Plotting two classes #####################################################
